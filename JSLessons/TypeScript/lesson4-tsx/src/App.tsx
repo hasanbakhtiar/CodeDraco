c81c690cb73ad7352119f52a6bc6ed0235175aab
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-10-18 20:45:04 +0400

    tsx useState

--- JSLessons/TypeScript/lesson4-tsx/src/App.tsx
+++ JSLessons/TypeScript/lesson4-tsx/src/App.tsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
 import ProductList from "./components/ProductList";
 import productData from "./data/productData";
 import { PropsType } from "./types/TypeConfig";
 
-const List =(props:PropsType)=>{
-  return(
+const List = (props: PropsType) => {
+  return (
     <ul>
       <li>{props.title}</li>
       <li>{props.price}</li>
     </ul>
-  )
-}
+  );
+};
 
+const UserData = () => {
+  return (
+    <>
+      <List title="BMW" price={10000} />
+      <ProductList product={productData} />
+    </>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <List title="BMW" price={10000} />
-      <ProductList product={productData}/>
-    </div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const handleLogin = ()=>{
+    setIsLoggedIn(true);
+  }
+  const handleLogout = ()=>{
+    setIsLoggedIn(false);
+  }
+  return <div className="App">
+    <div>User data:<div>
+    {isLoggedIn ? <UserData /> : 'logout user'}
+      </div></div>
+    <button onClick={handleLogin}>Log in</button>
+    <button onClick={handleLogout}>Log out</button>
+
+  </div>;
 }
 
 export default App;
